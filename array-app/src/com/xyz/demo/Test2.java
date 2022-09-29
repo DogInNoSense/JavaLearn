de2b6137c0319252d73513b5863a69a4a56c30c5
xyz <xieyinzhangl@gmail.com>
2022-09-29 21:16:53 +0800

    first commit

--- array-app/src/com/xyz/demo/Test2.java
+++ array-app/src/com/xyz/demo/Test2.java
@@ -2,4 +2,11 @@ package com.xyz.demo;
 
 public class Test2 {
     // 数组元素求最值
+    int faceScore[] = {15, 9000, 10000, 20000, 9500, -5};
+
+    int max = faceScore[0];
+
+    for(int i = 0;i < faceScore.length; i++){
+
+    }
 }
