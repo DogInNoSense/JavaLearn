79019be2e13f65866475eada92377de8d99b1f8e
xyz <xieyinzhangl@gmail.com>
2022-09-29 12:36:44 +0800

    first commit

--- flow-control-app/src/com/xyz/loop/DeadForDemo8.java
+++ flow-control-app/src/com/xyz/loop/DeadForDemo8.java
@@ -2,6 +2,12 @@ package com.xyz.loop;
 
 public class DeadForDemo8 {
     public static void main(String[] args) {
-
+        // 定义死循环
+//        for (int i = 0; ; i++) {
+//            System.out.println("hello world");
+//        }
+        while (true) {
+            System.out.println("死循环");
+        }
     }
 }
