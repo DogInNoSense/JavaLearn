60792f27ebb2e718a34a9c3269a180fefed40bd7
xyz <xieyinzhangl@gmail.com>
2022-09-29 17:44:39 +0800

    first commit

---
+++ array-app/src/com/xyz/create/ArrayDemo1.java
@@ -0,0 +1,16 @@
+package com.xyz.create;
+
+public class ArrayDemo1 {
+    public static void main(String[] args) {
+        // 学会使用静态初始化的方式定义数组
+//        double[] scores = new double[]{99.5, 88.8, 75.5};
+        double[] scores = {99.5, 88.8, 75.5};// 简化形式
+
+//        int[] ages = new int[]{12, 24, 36};
+        int[] ages = {12, 24, 36};
+
+//        String[] names = new String[]{"牛二", "全蛋", "老王"};
+        String[] names = {"牛二", "全蛋", "老王"};
+
+    }
+}
