132c468293f0852168ce3d99c2db579e7b645e2e
xyz <xieyinzhangl@gmail.com>
2022-09-29 20:56:45 +0800

    first commit

---
+++ array-app/src/com/xyz/traverse/ArrayDemo.java
@@ -0,0 +1,12 @@
+package com.xyz.traverse;
+
+public class ArrayDemo {
+    public static void main(String[] args) {
+        // 进行数组元素的遍历
+        int arr[] = {12, 24, 12, 48, 98};
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+}
