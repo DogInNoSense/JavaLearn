3358e1f229f519ab77b34b0daa8b629b74a0f2e7
xyz <xieyinzhangl@gmail.com>
2022-09-29 12:23:14 +0800

    first commit

---
+++ flow-control-app/src/com/xyz/loop/DoWhileDemo.java
@@ -0,0 +1,12 @@
+package com.xyz.loop;
+
+public class DoWhileDemo {
+    public static void main(String[] args) {
+        // 学会使用do while循环
+        int i = 0;
+        do {
+            System.out.println("hello world");
+            i++;
+        } while (i < 3);
+    }
+}
