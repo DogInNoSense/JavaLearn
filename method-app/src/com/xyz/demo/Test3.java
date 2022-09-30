5a00eb01fade323abb3bc3b2db99c049812679ce
xyz <xieyinzhangl@gmail.com>
2022-09-30 22:30:35 +0800

    first commit

--- method-app/src/com/xyz/demo/Test3.java
+++ method-app/src/com/xyz/demo/Test3.java
@@ -2,8 +2,18 @@ package com.xyz.demo;
 
 public class Test3 {
     public static void main(String[] args) {
-        // 数组求最值方法实现
-
+        // 整型数组求最值方法实现
+        int ages[] = {23, 19, 25, 78, 34};
+        System.out.println(getArrayMaxData(ages));//传入数组地址
     }
-    public static
+
+    public static int getArrayMaxData(int arr[]) {
+        int max = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+        return max;
+    }
 }
