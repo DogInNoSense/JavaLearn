178132bbd7aa62eee12f773bba311fab97c9d882
xyz <xieyinzhangl@gmail.com>
2022-10-05 21:59:10 +0800

    first commit

--- oop-app/src/com/xyz/javabean/Test.java
+++ oop-app/src/com/xyz/javabean/Test.java
@@ -2,6 +2,8 @@ package com.xyz.javabean;
 
 public class Test {
     public static void main(String[] args) {
+
+        // javabean 作用: 创建对象 封装数据 必须提供无参构造器
         // 1.无参构造器创建对象封装一个用户信息
         User u1 = new User();
         u1.setName("xyz");
@@ -12,7 +14,7 @@ public class Test {
         System.out.println(u1.getSalary());
 
         // 2.有参构造器创建对象封装一个用户信息
-        User u2 = new User("zyx",199,2000);
+        User u2 = new User("zyx", 199, 2000);
         System.out.println(u2.getName());
         System.out.println(u2.getHeight());
         System.out.println(u2.getSalary());
