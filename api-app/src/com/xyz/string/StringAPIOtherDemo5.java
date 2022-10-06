b13b5f6d651e358d1cd6e3db8515bad0944e109f
xyz <xieyinzhangl@gmail.com>
2022-10-06 21:19:48 +0800

    first commit

---
+++ api-app/src/com/xyz/string/StringAPIOtherDemo5.java
@@ -0,0 +1,14 @@
+package com.xyz.string;
+
+public class StringAPIOtherDemo5 {
+    public static void main(String[] args) {
+        // 1.public int length(): 获取字符串的长度
+        String name = "我爱你中国love";
+        System.out.println(name.length());
+        // 2.获取某个索引位置的字符
+        char c = name.charAt(1);
+        System.out.println(c);
+        // 3.遍历 每个字符
+
+    }
+}
